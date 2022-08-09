293ea38dfad80e2133fa1072326fb694eafb4b98
Caleb Bodtorf <cbodtorf@gmail.com>
2022-08-09 12:01:53 -0400

    fix: missed an update to backend prod env for frontendhost.

--- apps/backend/src/environments/environment.prod.ts
+++ apps/backend/src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
     appID: process.env.ALGOLIA_APP_ID,
     adminKey: process.env.ALGOLIA_ADMIN_API_KEY,
     indexName: process.env.ALGOLIA_INDEX_NAME
-  }
+  },
+  frontendHost: process.env.FRONTEND_HOST
 };
