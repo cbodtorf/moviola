3a80eeae64c1bbae9e14247f95ed3031d4c5fe34
Caleb Bodtorf <cbodtorf@gmail.com>
2022-08-09 02:59:13 -0400

    fix: missed update to delete component for env vars.

--- libs/util-schemas/src/lib/schemas.spec.ts
+++ libs/util-schemas/src/lib/schemas.spec.ts
@@ -33,10 +33,10 @@ describe('schemas', () => {
       expect(result).toEqual(true);
     });
 
-    it('should validate title', async () => {
+    it('should validate image', async () => {
       const result = await movieUpdateSchema.isValid({
         ...movieUpdateSchema,
-        title: 42
+        title: 'teset'
       });
       expect(result).toEqual(false);
     });
